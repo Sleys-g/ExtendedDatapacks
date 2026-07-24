bae7b3eb9d0acc7c772409d0a441d37556e908d7
Sleys <balthasarcardenas@gmail.com>
2026-07-24 13:37:23 -0500

    starting alignment

--- extended_datapacks-forge/src/main/java/sleys/efedp/system/animations/json/accessor/IAnimationAccessor.java
+++ extended_datapacks-forge/src/main/java/sleys/efedp/system/animations/json/accessor/IAnimationAccessor.java
@@ -5,7 +5,7 @@ import sleys.efedp.system.animations.json.properties.IAnimationProperty;
 import yesman.epicfight.api.animation.AnimationManager;
 import yesman.epicfight.api.animation.types.DynamicAnimation;
 
-public sealed interface IAnimationAccessor<T extends DynamicAnimation> permits ActionAnimationAccessor, AimAnimationAccessor, AirAttackAnimationAccessor, AttackAnimationAccessor, ComboAttackAccessor, DashAttackAnimationAccessor, DodgeAnimationAccessor, HitAnimationAccessor, KnockdownAnimationAccessor, LongHitAnimationAccessor, MovementAnimationAccessor, StaticAnimationAccessor {
+public sealed interface IAnimationAccessor<T extends DynamicAnimation> permits ActionAnimationAccessor, AimAnimationAccessor, AirAttackAnimationAccessor, AttackAnimationAccessor, ComboAttackAccessor, DashAttackAnimationAccessor, DodgeAnimationAccessor, GuardAnimationAccessor, HitAnimationAccessor, KnockdownAnimationAccessor, LongHitAnimationAccessor, MovementAnimationAccessor, StaticAnimationAccessor {
 
     AnimationRegistryType accessorType();
 
